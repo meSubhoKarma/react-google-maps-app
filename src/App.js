b50bed99de8e55cd893731c0d92d4481b76d36ad
Subho Karmakar <mesubhokarma@gmail.com>
2021-02-10 02:07:28 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// App.js
+import React from 'react';
+import './App.css';
+import GoogleMapComponent from './components/GoogleMapComponent';
+
+
+function App() {
+
+  return (
+    <div className="main-wrapper">
+      <GoogleMapComponent />
+    </div>
+  );
+}
+
+export default App;
